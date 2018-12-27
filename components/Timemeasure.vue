c86a0552b11b6035faf1e3bb099996dc9fa2caac
Sung Nahyun <mademoiselle@kookmin.ac.kr>
2018-12-27 21:05:21 +0900

    set up

---
+++ components/Timemeasure.vue
@@ -0,0 +1,40 @@
+<template>
+<section>
+  <div>
+    <div>그래프영역</div>
+    <div>태스크영역</div>
+  </div>
+</section>
+</template>
+
+<script>
+
+import AppLogo from '~/components/AppLogo.vue'
+import TimeMeasure from '../components/Timemeasure.vue';
+// import moduleName from '../components/Time';
+import Component from 'vue-class-component';
+import Vue from 'vue';
+
+@Component({
+components: {
+    AppLogo,
+    TimeMeasure
+  },
+  data:{
+    flag: null
+  },
+  methods: {
+    goTo: function(to) {
+      flag = to;
+    }
+  }
+})
+export default class Main extends Vue{
+  
+}
+</script>
+
+
+<style>
+</style>
+
